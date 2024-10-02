d5537b4246f3413d24418d53b40e75225449652e
강나연 <helpez@korea.ac.kr>
2024-10-02 23:30:19 +0900

    fixed

--- Louvain.py
+++ Louvain.py
@@ -1,7 +1,7 @@
 import network as nx
 import community
 G = nx.Graph()
-G.add_edges_from([(1,2), (1,3), (2,3)])
+G.add_edges_from([23])
 partition = community.best_partition(G)
 print("Node: Cluster") 
 for node, cluster in partition.items(): 
